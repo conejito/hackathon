d6e2236ea41de5b44a565a6c2e2cb3e98bdd21e5
Rafał Rudol <rrrrudol@gmail.com>
2017-11-18 23:58:53 +0100

    add simple webhook

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,8 @@ admin.initializeApp(functions.config().firebase);
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+exports.webhook = functions.https.onRequest((request, response) => {
+  db.insert(`webhook/logs/${Date().getTime()}`, request);
+  response.send('ok');
+});
