6a4c5ee7788e4e12b5e1c0049a4fea67fe86e8df
Tomasz Gil <tomasz.gil@akai.org.pl>
2017-11-18 17:30:36 +0100

    import assets

---
+++ src/components/conejito/conejito.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import './conejito.css';
+import conejito from './conejito.png';
+import conejitoBag from './conejitoBag.png';
+import conejitoSad from './conejitoSad.png';
