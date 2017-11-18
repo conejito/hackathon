119c118120e1ece87949a402d45fae029addfdaf
Marta Sitkowska <marta.sitkowska@akai.org.pl>
2017-11-18 22:34:06 +0100

    Carousel content

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 import './carousel.css';
 import Slider from 'react-slick';
+import Conejito from '../conejito/conejito'
 
 class Carousel extends Component {
   render() {
   let settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1
   };
   return (
     <div className='carousel'>
     <Slider {...settings}>
-      <div><h3>1</h3></div>
-      <div><h3>2</h3></div>
-      <div><h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3></div>
+      <div>
+        <Conejito type='bag' />
+        <h3>Często podróżujesz?</h3>
+        <p>To zupełnie jak nasz Conejito. Przybył on z Meksyku, by pomóc innym turystom w poznaniu nowego miejsca.</p>
+      </div>
+      <div>
+        <Conejito type='eat' />
+        <h3>Nie wiesz gdzie zjeść?</h3>
+        <p>Po prostu spytaj! Zastanawiałeś się gdzie jest najbliższa kawiarnia, albo do jakiej pizzerii udać się dzisiaj z rodziną? Teraz możesz w łatwy sposób poznać odpowiedź</p>
+      </div>
+      <div>
+        <Conejito type='popcorn' />
+        <h3>Szukasz rozrywki na wieczór?</h3>
+        <p>Kino, galeria sztuki, a może spa? Wybierz co chcesz i spytaj Conejito, a on przedstawi Ci możliwości, z nim na pewno coś znajdziesz :)</p>
+      </div>
     </Slider>
     </div>
   );
