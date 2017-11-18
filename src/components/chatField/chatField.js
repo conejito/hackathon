f332c181de0117e01724c226f795f75794b9eaef
Tomasz Gil <tomasz.gil@akai.org.pl>
2017-11-18 20:48:18 +0100

    landing elements styling

--- src/components/chatField/chatField.js
+++ src/components/chatField/chatField.js
@@ -1,3 +1,30 @@
 import React, { Component } from 'react';
 import './chatField.css';
 import mic from './mic.svg';
+
+
+class ChatField extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleMic = this.handleMic.bind(this);
+  }
+
+  handleMic() {
+
+  }
+
+  render() {
+    return (
+      <div className='chat-field'>
+        <div className='input-wrapper'>
+          <input className='input' placeholder="Masz jakieś pytanie?" />
+          <img src={mic} className='icon' onClick={this.handleMic} />
+        </div>
+        <div className='line' />
+      </div>
+    );
+  }
+}
+
+export default ChatField;
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './main.css';
 import Logo from '../../components/logo/logo';
 import Conejito from '../../components/conejito/conejito';
+import ChatField from '../../components/chatField/chatField';
+import Footer from '../../components/footer/footer';
 
 
 const Main = () => (
   <div className='main'>
     <Conejito type='bag' />
     <Logo variant='big' />
+    <ChatField />
+    <Footer />
   </div>
 );
 
