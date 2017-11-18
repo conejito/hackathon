03661bc8b04d66c3ee4e3514ce49ea7a63f6c99b
Tomasz Gil <tomasz.gil@akai.org.pl>
2017-11-18 22:02:55 +0100

    margin tweaks, loading overlay

--- src/components/loadingOverlay/loadingOverlay.js
+++ src/components/loadingOverlay/loadingOverlay.js
@@ -1,2 +1,19 @@
 import React, { Component } from 'react';
 import './loadingOverlay.css';
+import Conejito from '../../components/conejito/conejito';
+
+class LoadingOverlay extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="loadingOverlay">
+
+      </div>
+    );
+  }
+}
+
+export default LoadingOverlay;
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -3,12 +3,13 @@ import './main.css';
 import Logo from '../../components/logo/logo';
 import Conejito from '../../components/conejito/conejito';
 import ChatField from '../../components/chatField/chatField';
+// import LandingOverlay from '../../components/loadingOverlay/landingOverlay';
 import Footer from '../../components/footer/footer';
 
 
 const Main = () => (
   <div className='main'>
-    <Conejito type='bag' />
+    <Conejito type='beer' />
     <Logo variant='big' />
     <ChatField />
     <Footer />
