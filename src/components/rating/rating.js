9cb504c1a6947659967aeec462b5a7d66c557cf9
Piotr Ptak <piotr.ptak@akai.org.pl>
2017-11-19 04:18:32 +0100

    numeric value of rating

--- src/components/rating/rating.js
+++ src/components/rating/rating.js
@@ -6,12 +6,14 @@ import starEmpty from './starEmpty.svg';
 
 const Rating = (props) => (
   <div className="rating">
+    <span> {props.data} </span>
     {(Array(Math.floor(props.data)).fill(0)).map( (e, i) => <img key={i} src={starFull}/>)}
     {
       props.data > Math.floor(props.data) ?
       <img src={starHalf}/> : ''
     }
     {(Array(5-Math.ceil(props.data)).fill(0)).map( (e, i) => <img key={i} src={starEmpty}/>)}
+
   </div>
 );
 
